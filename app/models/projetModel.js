43c043780408e475b7d6711caa255c806abd22bd
CynthiaManier <cynthia.manier.pro@gmail.com>
2025-11-26 14:53:45 +0100

    fix: removal of an unrequired association

--- app/models/projetModel.js
+++ app/models/projetModel.js
@@ -7,65 +7,49 @@ import Preview from "./previewModel.js";
 
 class Projet extends Model {}
 
-export const STATUS = [ 
-  "à commencer",
-  "en cours",
-  "attente retour",
-  "terminé",
-  "attente acceptation"
+export const STATUS = [
+    "à commencer",
+    "en cours",
+    "attente retour",
+    "terminé",
+    "attente acceptation",
 ];
 
 Projet.init(
-  {
-    // ID unique du projet
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
+    {
+        // ID unique du projet
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        // Nom du projet
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        // Description du projet
+        resume: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        // Statut du projet
+        status: {
+            type: DataTypes.ENUM(...STATUS),
+            defaultValue: "attente acceptation",
+        },
+        // Date limite du projet
+        deadline: {
+            type: DataTypes.DATEONLY,
+            allowNull: true,
+        },
     },
-    // Nom du projet
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    // Description du projet
-    resume: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-  },
-    // Statut du projet
-    status: {
-      type: DataTypes.ENUM(...STATUS),
-      defaultValue: "attente acceptation",
-    },
-    // Date limite du projet
-    deadline: {
-      type: DataTypes.DATEONLY,
-      allowNull: true,
-    },
-  },
-  {
-    sequelize,
-    modelName: "Projet",
-    tableName: "projet",
-    timestamps: true,
-  }
+    {
+        sequelize,
+        modelName: "Projet",
+        tableName: "projet",
+        timestamps: true,
+    }
 );
-
-// ASSOCIATIONS :
-
-// Un projet appartient à un utilisateur
-Projet.belongsTo(User, { foreignKey: "user_id", as: "user" });
-// Un projet appartient à une entreprise
-Projet.belongsTo(Company, { foreignKey: "company_id", as: "company" });
-// Un projet peut avoir plusieurs genres & un genre peut appartenir à plusieurs projets
-Projet.belongsToMany(Genre, {
-  through: "projet_genre",
-  foreignKey: "projet_id",
-  otherKey: "genre_id",
-  as: "genres",
-});
-// Un projet peut avoir plusieurs extraits
-Projet.hasMany(Preview, { foreignKey: "projet_id", as: "previews" });
 
 export default Projet;
